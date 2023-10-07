92b1a42d215995b64a3f0f732fe30fd2e93164c7
Marius Paduraru <marius@yahpass.home>
2023-10-07 12:13:59 +0100

    feat: updating the DOM with cloned elements

--- scripts/index.js
+++ scripts/index.js
@@ -3,64 +3,90 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // is called when the DOM is loaded, builds the slider and adds the event listeners
   const buildSlider = (slider, visibleSlides) => {
+    // check if slider exists
+    if (!slider) return;
+
+    // get the slider elements
     const sliderTrack = slider.querySelector(".yp-slider__track");
     const slides = Array.from(slider.querySelectorAll(".yp-slider__slide"));
     const leftButton = slider.querySelector(".yp-slider__button-left");
     const rightButton = slider.querySelector(".yp-slider__button-right");
 
-    // add the first and last slide to the beginning and end of the slides array and update the slider track
-    const lastSlideIndex = slides.length - 1;
-    const firstSlide = slides[lastSlideIndex].cloneNode(true);
-    const lastSlide = slides[0].cloneNode(true);
-
-    const totalSlides = createTotalSlidesArray(slides, firstSlide, lastSlide);
-    updateSliderTrack(sliderTrack, totalSlides);
-
-    // set the slide width
-    const sliderWidth = slider.getBoundingClientRect().width;
-    const slideWidth = sliderWidth / visibleSlides;
-
-    // shift the slider track to the left by the width of one slide
-    shiftSliderTrack(totalSlides, sliderTrack, slideWidth);
+    // check if slider elements exist
+    if (!sliderTrack || !slides || !leftButton || !rightButton) return;
 
     let isAnimating = false;
 
+    // update the slider track by cloning the slides
+    const totalSlides = updateSliderTrack(slides, sliderTrack, visibleSlides);
+
+    // update the slide width to fit the slider track
+    updateSlideWidth(slider, totalSlides, visibleSlides);
+
+    // shift the slider track to the beginning
+    shiftSliderTrack(sliderTrack, slider);
+
     leftButton.addEventListener("click", () => {
       if (!isAnimating) {
-        isAnimating = true;
-        animateSliderTrack(sliderTrack, slideWidth, () => {
-          isAnimating = false;
-        });
+        //   isAnimating = true;
+        //   animateSliderTrack(sliderTrack, slideWidth, () => {
+        //     isAnimating = false;
+        //   });
       }
     });
 
     rightButton.addEventListener("click", () => {
       if (!isAnimating) {
-        isAnimating = true;
-        animateSliderTrack(sliderTrack, -slideWidth, () => {
-          isAnimating = false;
-        });
+        //   isAnimating = true;
+        //   animateSliderTrack(sliderTrack, -slideWidth, () => {
+        //     isAnimating = false;
+        //   });
       }
     });
   };
 
-  // creates an array of slides
-  const createTotalSlidesArray = (slides, firstSlide, lastSlide) => {
-    return [firstSlide, ...slides, lastSlide];
+  // updates the slider track with the new slides
+  const updateSliderTrack = (slides, sliderTrack, visibleSlides) => {
+    // create the total slides array by cloning the slides
+    const slidesLength = slides.length;
+    const startSlides = slides.map((slide) => slide.cloneNode(true));
+
+    const endSlides =
+      visibleSlides < slides.length
+        ? slides
+            .slice(0, slidesLength - visibleSlides)
+            .map((slide) => slide.cloneNode(true))
+        : slides.map((slide) => slide.cloneNode(true));
+
+    startSlides.reverse().forEach((slide) => {
+      slide.classList.add("yp-slider__slide--cloned");
+      sliderTrack.insertBefore(slide, sliderTrack.firstChild);
+    });
+
+    endSlides.forEach((slide) => {
+      slide.classList.add("yp-slider__slide--cloned");
+      sliderTrack.appendChild(slide);
+    });
+
+    const totalSlides = [...startSlides.reverse(), ...slides, ...endSlides];
+
+    return totalSlides;
   };
 
-  // updates the slider track with the new slides
-  const updateSliderTrack = (sliderTrack, totalSlides) => {
-    sliderTrack.innerHTML = "";
-    totalSlides.forEach((slide, index) => {
-      slide.dataset.index = index;
-      sliderTrack.appendChild(slide);
+  // updates the slide width to fit the slider track
+  const updateSlideWidth = (slider, slides, visibleSlides) => {
+    const sliderWidth = slider.getBoundingClientRect().width;
+    const slideWidth = sliderWidth / visibleSlides;
+
+    slides.forEach((slide) => {
+      slide.style.width = `${slideWidth}px`;
     });
   };
 
-  const shiftSliderTrack = (totalSlides, sliderTrack, slideWidth) => {
-    totalSlides.forEach((slide) => (slide.style.width = `${slideWidth}px`));
-    sliderTrack.style.transform = `translateX(-${slideWidth}px)`;
+  // shifts the slider track by the given offset
+  const shiftSliderTrack = (sliderTrack, slider) => {
+    sliderTrack.style.transform = `translateX(${-slider.getBoundingClientRect()
+      .width}px)`;
   };
 
   const animateSliderTrack = (
