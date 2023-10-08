a2203507898a36ebc70a786bbe673fb97c41e77f
Marius Paduraru <marius@yahpass.home>
2023-10-08 06:37:47 +0100

    feat: updating the slides

--- scripts/index.js
+++ scripts/index.js
@@ -11,13 +11,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const slides = Array.from(slider.querySelectorAll(".yp-slider__slide"));
 
     // get the slider buttons
-    const [leftButton, rightButton] = [
-      ".yp-slider__button-left",
-      ".yp-slider__button-right",
+    const [prevButton, nextButton] = [
+      ".yp-slider__button-prev",
+      ".yp-slider__button-next",
     ].map((selector) => slider.querySelector(selector));
 
     // check if slider elements exist
-    if (!sliderTrack || !slides || !leftButton || !rightButton) return;
+    if (!sliderTrack || !slides || !prevButton || !nextButton) return;
 
     // check if the visibleSlides is less than the total slides the visible slides should be the total slides
     if (visibleSlides > slides.length) visibleSlides = slides.length;
@@ -28,28 +28,31 @@ document.addEventListener("DOMContentLoaded", () => {
     // initialize the slider
     initSlider(slides, sliderTrack, visibleSlides);
 
-    // add event listeners the left button
-    leftButton.addEventListener("click", () => {
-      // check the current index
-      currentIndex === 0 ? (currentIndex = slides.length - 1) : currentIndex--;
+    // set and array with the total slides
+    let totalSlides = Array.from(sliderTrack.children);
 
-      // remove the remove the last slide from the DOM and add a new one at the beginning based on the currentIndex
-      shiftSlider(currentIndex, "right");
+    // add event listeners the left button
+    prevButton.addEventListener("click", () => {
+      currentIndex === 0
+        ? (currentIndex = totalSlides.length - 1)
+        : currentIndex--;
+
+      // remove the last slide from the DOM and add a new one at the beginning based on the currentIndex
+      shiftSlider(totalSlides, 1);
     });
 
     // add event listeners the right button
-    rightButton.addEventListener("click", () => {
-      // check the current index
-      currentIndex === slides.length - 1 ? (currentIndex = 0) : currentIndex++;
+    nextButton.addEventListener("click", () => {
+      currentIndex === totalSlides.length - 1
+        ? (currentIndex = 0)
+        : currentIndex++;
 
-      // remove the remove the first slide and add a new one at the end based on the currentIndex
-      shiftSlider(currentIndex, "left");
+      // remove the first slide and add a new one at the end based on the currentIndex
+      shiftSlider(totalSlides, -1);
     });
   };
 
-  const shiftSlider = (currentIndex, direction) => {
-    console.log("shiftSlider", currentIndex, direction);
-  };
+  const shiftSlider = (slides, direction) => {};
 
   // get the slide width
   const getSlideWidth = (slider, visibleSlides) => {
@@ -59,22 +62,26 @@ document.addEventListener("DOMContentLoaded", () => {
     return slideWidth;
   };
 
+  const cloneSlides = (slides, sliderTrack, visibleSlides) => {
+    const firstSlide = slides[slides.length - 1].cloneNode(true);
+    sliderTrack.prepend(firstSlide);
+    const lastSlide = !(visibleSlides < slides.length)
+      ? slides[0].cloneNode(true)
+      : null;
+
+    lastSlide && sliderTrack.append(lastSlide);
+    const totalSlides = [firstSlide, ...slides, lastSlide];
+
+    return totalSlides;
+  };
+
   // clone the first and last slide and add them to the slider,
   // set the width of the slides and set the initial position
   // of the slider track
   const initSlider = (slides, sliderTrack, visibleSlides) => {
     const slideWidth = getSlideWidth(slider, visibleSlides);
 
-    const firstSlide = slides[slides.length - 1].cloneNode(true);
-    sliderTrack.prepend(firstSlide);
-
-    const lastSlide = !(visibleSlides < slides.length)
-      ? slides[0].cloneNode(true)
-      : null;
-
-    lastSlide && sliderTrack.append(lastSlide);
-
-    const totalSlides = [firstSlide, ...slides, lastSlide];
+    const totalSlides = cloneSlides(slides, sliderTrack, visibleSlides);
 
     totalSlides.forEach((slide) => {
       slide.style.width = `${slideWidth}px`;
